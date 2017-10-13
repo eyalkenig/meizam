a9ecd3467b312d12d2e4d55a4c71764fe36a9e46
eyalkenig <eyalkenig@gmail.com>
2017-10-13 15:26:24 +0300

    feat(ApiService):added test

---
+++ src/app/services/api.service.spec.ts
@@ -0,0 +1,37 @@
+import { TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ApiService } from "app/services/api.service";
+import { HttpTestingController } from "@angular/common/http/testing";
+import { inject } from "@angular/core/testing";
+import { environment } from "environments/environment";
+
+describe("ApiService", () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        ApiService
+      ]
+    });
+  });
+
+  describe("fetchNextFixtures", () => {
+    it("should get the next fixtures from backend url configured in environment", function () {
+      inject([ApiService, HttpTestingController],
+          (apiService: ApiService, httpMock: HttpTestingController) => {
+            environment.backend_url = "http://the/backend/url"
+
+            apiService.fetchNextFixtures().subscribe(res => {});
+
+            const req = httpMock.expectOne("http://the/backend/url/next-fixtures");
+            expect(req).toBeDefined();
+            expect(req.request.method).toEqual("GET");
+            req.flush("");
+            httpMock.verify();
+          })();
+    });
+  });
+});
