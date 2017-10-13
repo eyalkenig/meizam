0ba33586548d4c25a251b992736591e6528661fd
eyalkenig <eyalkenig@gmail.com>
2017-10-13 15:25:50 +0300

    feat(NextFeatureService):added tests

---
+++ src/app/services/next-fixtures.service.spec.ts
@@ -0,0 +1,78 @@
+import { TestBed } from "@angular/core/testing";
+import { ApiService } from "app/services/api.service";
+import { ApiServiceMock } from "app/services/mocks/api.service.mock";
+import { NextFixturesActions } from "app/components/containers/next-fixtures/next-fixtures.actions";
+import { NgReduxTestingModule, MockNgRedux } from "@angular-redux/store/testing";
+import { NextFixturesService } from "app/services/next-fixtures.service";
+import { inject } from "@angular/core/testing";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/of";
+import { Fixture, AppState } from "app/app.state";
+import { NgRedux } from "@angular-redux/store";
+
+describe("NextFixturesService", () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        NextFixturesService,
+        NgReduxTestingModule,
+        NextFixturesActions,
+        {
+          provide: NgRedux,
+          useClass: MockNgRedux
+        },
+        {
+          provide: ApiService,
+          useClass: ApiServiceMock
+        }
+      ]
+    });
+  });
+
+  describe("fetchNextFixtures", () => {
+    it("should fetch next fixtures from api service", function () {
+      inject([NextFixturesService, ApiService],
+          (nextFixturesService: NextFixturesService, apiService: ApiService) => {
+            spyOn(apiService, "fetchNextFixtures").and.returnValue(Observable.of({}));
+
+            nextFixturesService.fetchNextFixtures();
+
+            expect(apiService.fetchNextFixtures).toHaveBeenCalled();
+          })();
+    });
+    it("should dispatch fetch next fixtures success on success", function () {
+      inject([NextFixturesService, ApiService, NextFixturesActions, NgRedux],
+        (nextFixturesService: NextFixturesService, apiService: ApiService, nextFixturesActions: NextFixturesActions,
+          mockNgRedux: MockNgRedux) => {
+          const expectedResult: Fixture[] = [{
+            leauge: { name: "a league", started_at: new Date() },
+            time: new Date(),
+            homeTeam: { name: "a team", logoUrl: "logo1" },
+            awayTeam: { name: "b team", logoUrl: "logo2" }
+          }];
+          const expectedAction = nextFixturesActions.fetchNextFixturesSuccess(expectedResult);
+          spyOn(apiService, "fetchNextFixtures").and.returnValue(Observable.of(expectedResult));
+          const actualDispatch = spyOn(mockNgRedux, "dispatch");
+
+          nextFixturesService.fetchNextFixtures();
+
+          expect(actualDispatch.calls.mostRecent().args[0]).toEqual(expectedAction);
+      })();
+    });
+    it("should dispatch fetch next fixtures fail on failure", function () {
+      inject([NextFixturesService, ApiService, NextFixturesActions, NgRedux],
+        (nextFixturesService: NextFixturesService, apiService: ApiService, nextFixturesActions: NextFixturesActions,
+          mockNgRedux: MockNgRedux) => {
+          const expectedError = "the error";
+          const expectedAction = nextFixturesActions.fetchNextFixturesFail(expectedError);
+          spyOn(apiService, "fetchNextFixtures").and.returnValue(Observable.create(observer => { observer.error(expectedError) }));
+          const actualDispatch = spyOn(mockNgRedux, "dispatch");
+
+          nextFixturesService.fetchNextFixtures();
+
+          expect(actualDispatch.calls.mostRecent().args[0]).toEqual(expectedAction);
+      })();
+    });
+  });
+});
